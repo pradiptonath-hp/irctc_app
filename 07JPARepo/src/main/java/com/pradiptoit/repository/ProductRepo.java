c6bcb0acb4e135c206d62f3d57cfc458349abe4a
pradiptonath-hp <69723748+pradiptonath-hp@users.noreply.github.com>
2023-09-20 00:54:49 +0530

    Delete 07JPARepo directory

--- 07JPARepo/src/main/java/com/pradiptoit/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pradiptoit.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pradiptoit.entities.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-
-}
